7957c19f1ccf23e741c1d067786dc4f1057260d7
dm4th <danny.mathieson233@gmail.com>
2023-03-31 16:08:04 -0700

    Did a lot of work with GPT4 trying to figure out Contentful headless CMS

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import Layout from '../../components/layout';
 import Date from '../../components/date';
+import ContentWrapper from '../../components/ContentWrapper';
 import utilStyles from '../../styles/utils.module.css';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import richTextRendererOptions from '../../lib/richTextRenderer';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -14,7 +17,7 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds();
+  const paths = await getAllPostIds();
   return {
     paths,
     fallback: false
@@ -30,9 +33,25 @@ export default function Post({ postData }) {
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
-          <Date dateString={postData.date}/>
+          <Date dateString={postData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+        <div>
+          <h1>Intro</h1>
+          {documentToReactComponents(postData.intro, richTextRendererOptions)}
+        </div>
+        <div>
+          <h1>Takeaways</h1>
+          {documentToReactComponents(postData.takeaways, richTextRendererOptions)}
+        </div>
+        <div>
+          <h1>Contents</h1>
+          {documentToReactComponents(postData.contents, richTextRendererOptions)}
+        </div>
+        <div>
+          <ContentWrapper utilStyles={utilStyles} richTextRendererOptions={richTextRendererOptions}>
+            {documentToReactComponents(postData.body, richTextRendererOptions)}
+          </ContentWrapper>
+        </div>
       </article>
     </Layout>
   );
