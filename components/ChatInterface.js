5c2c420d87675506e3e88b5cdf83f263965c138c
dm4th <danny.mathieson233@gmail.com>
2023-04-29 13:04:16 -0700

    reacclimation

---
+++ components/ChatInterface.js
@@ -0,0 +1,11 @@
+// React imports
+import React, { useState, useEffect } from 'react';
+
+// Networking Imports
+import { fetchEventSource } from '@microsoft/fetch-event-source';
+
+const ChatInterface = ({ role, onChangeRole }) => {
+    
+};
+
+export default ChatInterface;
