b4c0965153cc1b3e1ba91c491bceab2f73b6fceb
dm4th <danny.mathieson233@gmail.com>
2023-04-30 21:02:09 -0700

    setting u[ state variables for chat interface

--- components/ChatInterface.js
+++ components/ChatInterface.js
@@ -4,8 +4,19 @@ import React, { useState, useEffect } from 'react';
 // Networking Imports
 import { fetchEventSource } from '@microsoft/fetch-event-source';
 
-const ChatInterface = ({ role, onChangeRole }) => {
+const ChatInterface = ({ onChangeRole }) => {
+    const [role, setRole] = useState('none');
+    const [initialized, setInitialized] = useState(false);
+    const [messages, setMessages] = useState([]);
+    const [latestUserMessage, setLatestUserMessage] = useState('');
+    const [latestResponse, setLatestResponse] = useState('');
+
     
+
+    return (
+        <div>
+        </div>
+    );
 };
 
 export default ChatInterface;
